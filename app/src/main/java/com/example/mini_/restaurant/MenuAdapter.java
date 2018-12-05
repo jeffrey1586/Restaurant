e1fde34c84d3f71b977b5dd05429b5002178334c
Jeffrey Chong <jeffreychong69@gmail.com>
2018-12-05 12:02:39 +0100

    "final"

--- app/src/main/java/com/example/mini_/restaurant/MenuAdapter.java
+++ app/src/main/java/com/example/mini_/restaurant/MenuAdapter.java
@@ -6,12 +6,19 @@ import android.graphics.BitmapFactory;
 import android.net.Uri;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
 import android.widget.ImageView;
 import android.widget.TextView;
+
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.ImageRequest;
+import com.android.volley.toolbox.Volley;
 
 import java.io.IOException;
 import java.io.InputStream;
@@ -22,10 +29,12 @@ import java.util.List;
 public class MenuAdapter extends ArrayAdapter<MenuItem> {
 
     ArrayList<MenuItem> listItems;
+    Context currContext;
 
     public MenuAdapter(@NonNull Context context, int resource, @NonNull ArrayList<MenuItem> objects) {
         super(context, resource, objects);
         this.listItems = objects;
+        this.currContext = context;
     }
 
     @NonNull
@@ -36,14 +45,29 @@ public class MenuAdapter extends ArrayAdapter<MenuItem> {
                     (R.layout.menu_rows, parent, false);
         }
 
-//        ImageView imageUrl = convertView.findViewById(R.id.imageUrl);
+        RequestQueue queue = Volley.newRequestQueue(currContext);
+        final ImageView imageUrl = convertView.findViewById(R.id.imageUrl);
         TextView rowTitle = convertView.findViewById(R.id.rowTitle);
         TextView rowPrice =convertView.findViewById(R.id.rowPrice);
 
-//        String image = listItems.get(position).getImageUrl();
+        String image = listItems.get(position).getImageUrl();
         String title = listItems.get(position).getName();
         Long price = listItems.get(position).getPrice();
 
+        ImageRequest imageRequest = new ImageRequest(image, new Response.Listener<Bitmap>() {
+            @Override
+            public void onResponse(Bitmap response) {
+                Log.d("MenuItemActivity", "onResponse");
+                imageUrl.setImageBitmap(response);
+            }
+        }, 0, 0, null, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                Log.d("MenuItemActivity", "onErrorResponse");
+                imageUrl.setImageResource(R.drawable.ic_launcher_background);
+            }
+        });
+        queue.add(imageRequest);
 
         rowTitle.setText(title);
         rowPrice.setText("$ " + String.valueOf(price));
--- app/src/main/java/com/example/mini_/restaurant/MenuItemActivity.java
+++ app/src/main/java/com/example/mini_/restaurant/MenuItemActivity.java
@@ -1,35 +1,67 @@
 package com.example.mini_.restaurant;
 
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.widget.ImageView;
 import android.widget.TextView;
 
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.ImageLoader;
+import com.android.volley.toolbox.ImageRequest;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.Volley;
+
 import org.w3c.dom.Text;
 
+import java.io.IOException;
+import java.net.MalformedURLException;
+import java.net.URL;
+
 public class MenuItemActivity extends AppCompatActivity {
+
+    ImageView itemImage;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_menu_item);
 
+        RequestQueue queue = Volley.newRequestQueue(this);
         Intent intent = getIntent();
         MenuItem selectedItem = (MenuItem) intent.getSerializableExtra("selected_item");
 
         String title = selectedItem.getName();
-//        String image = selectedItem.getImageUrl();
+        String image = selectedItem.getImageUrl();
         String description = selectedItem.getDescription();
         Long price = selectedItem.getPrice();
 
         TextView itemTitle = findViewById(R.id.itemTitle);
-//        ImageView itemImage = findViewById(R.id.itemImage);
+        itemImage = findViewById(R.id.itemImage);
         TextView itemDescription = findViewById(R.id.itemContent);
         TextView itemPrice = findViewById(R.id.itemPrice);
 
+        ImageRequest imageRequest = new ImageRequest(image, new Response.Listener<Bitmap>() {
+            @Override
+            public void onResponse(Bitmap response) {
+                Log.d("MenuItemActivity", "onResponse");
+                itemImage.setImageBitmap(response);
+            }
+        }, 0, 0, null, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                Log.d("MenuItemActivity", "onErrorResponse");
+                itemImage.setImageResource(R.drawable.ic_launcher_background);
+            }
+        });
+        queue.add(imageRequest);
+
         itemTitle.setText(title);
-//        itemImage.set();
         itemDescription.setText(description);
         itemPrice.setText("$ " + String.valueOf(price));
     }
